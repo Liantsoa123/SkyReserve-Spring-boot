dd83d912f4e3876fe99d5e4cf736c24f93c9ea70
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-03-14 08:45:57 +0300

    Initial commit

---
+++ src/main/java/mg/noob/SkyReserve_Spring_boot/SkyReserveSpringBootApplication.java
@@ -0,0 +1,13 @@
+package mg.noob.SkyReserve_Spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyReserveSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyReserveSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/noob/SkyReserve_Spring_boot/SkyReserveSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.noob.SkyReserve_Spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyReserveSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
