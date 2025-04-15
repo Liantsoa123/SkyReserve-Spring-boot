7b2c518c8405e2e6e8948bacc7b23a7a85455088
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-04-15 21:12:34 +0300

    feat: Add ReservationStatus entity with fields for id and reservation name.

---
+++ src/main/java/mg/noob/SkyReserve_Spring_boot/model/ReservationStatus.java
@@ -0,0 +1,24 @@
+package mg.noob.SkyReserve_Spring_boot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reservation_status")
+public class ReservationStatus {
+    @Id
+    @ColumnDefault("nextval('reservation_status_reservation_status_id_seq')")
+    @Column(name = "reservation_status_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "reseravtion_name", nullable = false, length = 250)
+    private String reseravtionName;
+
+}
