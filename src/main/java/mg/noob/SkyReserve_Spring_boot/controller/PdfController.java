824104a78765c90d1e35984e16839b8d0509ea75
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-04-15 21:33:19 +0300

    feat: Enable CORS for PdfController to allow cross-origin requests

--- src/main/java/mg/noob/SkyReserve_Spring_boot/controller/PdfController.java
+++ src/main/java/mg/noob/SkyReserve_Spring_boot/controller/PdfController.java
@@ -1,19 +1,24 @@
 package mg.noob.SkyReserve_Spring_boot.controller;
 
 import com.itextpdf.text.DocumentException;
+import mg.noob.SkyReserve_Spring_boot.model.Reservation;
+import mg.noob.SkyReserve_Spring_boot.repository.ReservationRepository;
 import mg.noob.SkyReserve_Spring_boot.service.PdfGenerationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/pdf")
+@CrossOrigin(origins = "*")
 public class PdfController {
 
     @Autowired
